3be54b4ce584a15ad2546c0daad93d0f1cc429f2
Vadim <vsokolvak2000@gmail.com>
2024-09-01 20:09:22 +0300

    make step 2

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return error;
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,17 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { readContacts } from './readContacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    let data = await readContacts();
+    data.push(updatedContacts);
+    data = JSON.stringify(data);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    console.log('sucsess');
+    return 'sucsess';
+  } catch (error) {
+    console.log('error');
+    return error;
+  }
+};
